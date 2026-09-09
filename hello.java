d5d4f8094acfa45a8aee26488e2a4d0ec9b9ea13
Sumanth varma <sumanthvarma250@gmail.com>
2026-09-09 10:23:28 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+package ciet;
+
+public class hello {
+	public static void main(String [] args) {
+		int[] arr = {10, 20, 30, 40, 50};
+		int value = arr[2];
+		System.out.println("Value = " + value);
+	}
+
+}
